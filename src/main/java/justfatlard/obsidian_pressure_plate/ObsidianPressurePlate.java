42cc20c1aa667e3269f25fdfd055df32dcee8566
fatlard1993 <fatlard1993>
2020-04-20 00:47:52 -0700

    fix erronous collision box, fix other entities able to hold plate down

--- src/main/java/justfatlard/obsidian_pressure_plate/ObsidianPressurePlate.java
+++ src/main/java/justfatlard/obsidian_pressure_plate/ObsidianPressurePlate.java
@@ -1,13 +1,19 @@
 package justfatlard.obsidian_pressure_plate;
 
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
+import java.util.Iterator;
+import java.util.List;
+
 import net.fabricmc.fabric.api.block.FabricBlockSettings;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
+import net.minecraft.world.World;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Material;
 import net.minecraft.block.PressurePlateBlock;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.EntityType;
 
 interface CollisionTask { void run(BlockState state, World world, BlockPos pos, Entity entity); }
 
@@ -18,7 +24,7 @@ public class ObsidianPressurePlate extends PressurePlateBlock {
 	public CollisionValidator collisionValidator = PlayerEntity.class::isInstance;
 
 	public ObsidianPressurePlate(){
-		super(ActivationRule.EVERYTHING, FabricBlockSettings.of(Material.STONE).strength(0.6F, 26.0F).build());
+		super(ActivationRule.EVERYTHING, FabricBlockSettings.of(Material.STONE).noCollision().strength(0.6F, 26.0F).build());
 	}
 
 	@Override
@@ -34,5 +40,24 @@ public class ObsidianPressurePlate extends PressurePlateBlock {
 				if(collisionTask != null) collisionTask.run(state, world, pos, entity);
 			}
 		}
+	}
+
+	@Override
+	protected int getRedstoneOutput(World world, BlockPos pos) {
+		Box box = BOX.offset(pos);
+		List list3 = world.getEntities((Entity)null, box);
+
+		if (!list3.isEmpty()) {
+			Iterator var5 = list3.iterator();
+
+			while(var5.hasNext()) {
+				Entity entity = (Entity)var5.next();
+				if (entity.getType() == EntityType.PLAYER){
+					return 15;
+				}
+			}
+		}
+
+		return 0;
 	}
 }
